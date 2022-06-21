e45169f2aea921463b7846481626ceeb313ea72f
Jesús Ugaz Vegas <jesusugazv@gmail.com>
2022-06-21 12:36:47 -0500

    Front Navegacion Dinamica

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Typography",
+    caption: "Tipos de letras de Quasar",
+    icon: "lab la-fort-awesome-alt",
+    link: "typography",
+  },
+  {
+    title: "Flex Layout",
+    caption: "Estilos con Flex",
+    icon: "las la-layer-group",
+    link: "flex",
+  },
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "las la-graduation-cap",
+    link: "https://quasar.dev",
+  },
+];
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/Flex.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex">
+    <h1>Flex.vue</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FlexPage",
+});
+</script>
